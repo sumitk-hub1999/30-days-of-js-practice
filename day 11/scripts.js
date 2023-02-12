d06d4fd0820e0f00585e8fc87b859869e0147723
sumitk-hub1999 <skatyal1999@gmail.com>
2023-02-12 09:47:54 +0530

    fullscreen button remaining

--- day 11/scripts.js
+++ day 11/scripts.js
@@ -14,18 +14,40 @@ function togglePlay() {
   //   } else {
   //     video.pause();
   //   }
-}
+} // data skip attribute
 function updateButton() {
   console.log("update the button");
-  const icon = this.paused ? "💥" : "💙";
+  const icon = this.paused ? "💥" : "💙"; //we use this coz its bound by the video container
   toggle.textContent = icon;
 }
 function skip() {
   console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
 }
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
+function handleProgressBar() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e) {
+  console.log(e);
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+//hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgressBar);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
